4b8f881f20a03bc2f7b8605ddfed73ad98e07762
dmytroliashok <dmytro.crypto99@gmail.com>
2025-04-15 13:23:41 -0700

    test update

--- src/config/index.ts
+++ src/config/index.ts
@@ -34,8 +34,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 export const X_API_KEY: string = process.env.X_API_KEY || "";
 
 export const connection = new Connection(SOLANA_RPC_URL, {
-  wsEndpoint: WSS_URL,
-  commitment: "processed"
+  wsEndpoint: WSS_URL
 });
 
 export const metaplex = new Metaplex(connection);
